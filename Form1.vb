005168a2048fb9bd1bf5332a35d7e345d01fd779
HermanceP <38039057+HermanceP@users.noreply.github.com>
2019-02-09 18:54:06 +0100

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextResultat_TextChanged(sender As Object, e As EventArgs) Handles TextResultat.TextChanged
+
+    End Sub
+End Class
